6106d76aad5067bd04860e8ba6bdd608e5a16f71
davidmae <david.mae20@gmail.com>
2020-03-23 22:24:29 +0100

    no message

---
+++ Assets/EffectorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectorController : MonoBehaviour
+{
+    public float offsetx;
+    public float offsety;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        GetComponent<LineRenderer>().SetPosition(1, new Vector3(transform.position.x + offsetx, transform.position.y + offsety, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
